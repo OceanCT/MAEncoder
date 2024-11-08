1bd921f0520d7a91e37b7db82863dd3dac4b59db
OceanCT <88697499+OceanCT@users.noreply.github.com>
2024-11-08 15:39:39 +0800

    Update mae.cpp
    Fix Decompress issue

--- mae.cpp
+++ mae.cpp
@@ -6,13 +6,16 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <unistd.h>
 typedef uint8_t U8;
 typedef uint16_t U16;
 typedef uint32_t U32;
 typedef uint64_t U64;
-
-const std::string file_path = "./szdat2";
-const int n = 100000000 / 4;
+const std::string file_path = "./szdat";
+// const std::string file_path = "./data_pre/CESM-ATM/szdats/CLOUD_1_26_1800_3600.f32.sz4.szdat";
+// const int n = 100000000 / 4;
+// const int n = 673920000 / 4;
+const int n = 2869440;
 // const int n = 2869440;
 // const int n = 10;
 const int DEBUG = 1;
@@ -35,7 +38,7 @@ struct PredictTree {
         depth = bitwidth;
         table.resize(1 << (depth + 1), 0);
         pos = 1;
-        curdepth = 1;
+        curdepth = 0;
     }
     int get_currentprob() {
         int p1 = table[pos * 2 + 1];
@@ -63,7 +66,6 @@ struct SizedPredictTree {
     int pos; // [0, depth]
     int maxsize;
     SizedPredictTree(int bitwidth = 16, int maxsize = 1024) {
-        // need 2^(depth + 1) byte num
         depth = bitwidth;
         table.resize(1 << (depth + 1), 0);
         this->maxsize = maxsize;
@@ -73,6 +75,7 @@ struct SizedPredictTree {
     int get_currentprob() {
         int p1 = table[pos * 2 + 1];
         int p2 = table[pos * 2];
+        // printf("p1: %d, p2: %d\n", p1, p2);
         assert(p1 >= 0 && p2 >= 0);
         if(p1 + p2 == 0) return 2048;
         return 4095 * ((double) p1 / (p1 + p2));
@@ -99,11 +102,13 @@ struct SizedPredictTree {
         for(int i = depth - 1; i >= 0; i--) {
             now = now * 2 + ((x >> i) & 1);
             table[now]--;
+            assert(table[now] >= 0);
         }
     }
 
 };
 
+int printcnt = 0;
 struct Predictor {
     int pr, pr1, pr2;
     int w1, w2; // [0-10]
@@ -116,13 +121,16 @@ struct Predictor {
         buffer(0), bitwidth(bitwidth), 
         tree(new PredictTree(bitwidth)), stree(new SizedPredictTree(bitwidth, 1024)),
         w1(1), w2(1) {}
-
+    void init() {
+        stree->depth = bitwidth;
+        tree->depth = bitwidth;
+    }
     bool update_bit(int y) {
         stree->updatebit(y);
         tree->updatebit(y);
         int ret = (pr > 2048 && y) || (pr < 2048 && !y);
-        int r1 = (pr1 > 2048 && y) || (pr1 < 2048 && !y);
-        int r2 = (pr2 > 2048 && y) || (pr2 < 2048 && !y);
+        bool r1 = (pr1 > 2048 && y) || (pr1 < 2048 && !y);
+        bool r2 = (pr2 > 2048 && y) || (pr2 < 2048 && !y);
         if(r1) {
             if(w1 > 3) w1 <<= 1;
             else w1++;
@@ -135,51 +143,51 @@ struct Predictor {
         } else {
             if(w2 > 1) w2--;
         }
-        // dprintf("predict: %d, actual bit: %d\n", pr, y);
         bitpos++;
         buffer += buffer + y;
         if(bitpos == bitwidth) {
             stree->add(buffer);
-            w1 = w2 = 1;
+            w1 = w2 = 2;
             bitpos = 0;       
             buffer = 0;
         }
         return ret;
     }
-    int predict_bit() {
-        int pr1 = stree->get_currentprob();
-        int pr2 = tree->get_currentprob();
+    int predict_bit() { 
+        pr1 = stree->get_currentprob();
+        pr2 = tree->get_currentprob();
         pr = (pr1 * w1 + pr2 * w2) / (w1 + w2);
-        // dprintf("predict: %d\n", pr);
         assert(pr >= 0 && pr < 4096);
         return pr;
     }
 };
 
+int cnt = 0;
+int bitwidth = 0;
+
 struct Encoder {
     U8* origin; int fb; 
-    U32 x1, x2;    
+    U32 x1, x2, x;    
     int bitwidth; 
     U32 correctcnt;
-    Predictor* predictor;
-    U32 x;      
-    Encoder(int bitwidth = 16) {
+    struct Predictor predictor;  
+    Encoder(int bitwidth = 32) {
         this->bitwidth = bitwidth;
         x1 = 0, x2 = 0xffffffff, x = 0;
         correctcnt = 0; fb = 0;
-        predictor = new Predictor(bitwidth);
+        predictor.bitwidth = bitwidth;
+        predictor.init();
     }    
 
     int code(int y = 0) {
-        // dprintf("codebit %d\n", y);
-        int p = predictor->predict_bit();
-        // dprintf("codebitx %d, p: %d\n", y, p);
+        // int p = 1017;
+        int p = predictor.predict_bit();
         assert(p >= 0 && p < 4096);
         p += p < 2048;
         U32 xmid = x1 + ((x2 - x1) >> 12) * p + ((x2 - x1 & 0xfff) * p >> 12);
         assert(xmid >= x1 && xmid < x2);
-        correctcnt += predictor->update_bit(y);
         y ? (x2 = xmid) : (x1 = xmid + 1);
+        correctcnt += predictor.update_bit(y);
         while (((x1 ^ x2) & 0xff000000) == 0) {
             origin[fb++] = x2 >> 24;
             x1 <<= 8;
@@ -188,14 +196,15 @@ struct Encoder {
         return y;
     }
     int decode(int y = 0) {
-        int p = predictor->predict_bit();
+        // int p = 1017;
+        int p = predictor.predict_bit();
         assert(p >= 0 && p < 4096);
         p += p < 2048;
         U32 xmid = x1 + ((x2 - x1) >> 12) * p + ((x2 - x1 & 0xfff) * p >> 12);
         assert(xmid >= x1 && xmid < x2);
-        correctcnt += predictor->update_bit(y);
         y = x <= xmid;
         y ? (x2 = xmid) : (x1 = xmid + 1);
+        correctcnt += predictor.update_bit(y);
         while (((x1 ^ x2) & 0xff000000) == 0) {
             x1 <<= 8;
             x2 = (x2 << 8) + 255;
@@ -217,66 +226,29 @@ struct Encoder {
     }
 };
 
-int bitwidth, base, basepos;
 U32 successcnt = 0;
-
 void compress(int *origin, size_t length, U8** res, int* res_len) {
-    // dprintf("length: %ld\n", length);
-    std::map<int, int> mp;
     bitwidth = 0;
+    int maxx = origin[0];
     for(int i = 0; i < length; i++) {
-        mp[origin[i]]++;
-        // dprintf("origin[%d]: %d\n", i, origin[i]);
-    }
-    auto t = mp.end();
-    t--;
-    int maxx = t->first;
-    int base = mp.begin()->first;
-    unsigned int tmp = maxx - base;
-    while(tmp) {
-        bitwidth++;
-        tmp >>= 1;
-    }
-    // if(base != 0) {
-        for(int i = 0; i < length; i++) {
-            origin[i] -= base;
+        if(origin[i] > maxx) {
+            maxx = origin[i];
         }
-    // } else {
-    //     int k = mp.begin()->first;
-    //     int knum = mp.begin()->second;
-    //     for(auto s: mp) {
-    //         if(s.second > knum) {
-    //             k = s.first;
-    //             knum = s.second;
-    //         }
-    //     }
-    //     // t == 0 -> t =  k - 1
-    //     // t < k -> t = t - 1
-    //     // t > k -> t = t
-    //     for(int i = 0; i < length; i++) {
-    //         if(!origin[i]) {
-    //             origin[i] = k - 1;
-    //         } else if(origin[i] < k) {
-    //             origin[i]--;
-    //         }
-    //     }
-    // }
-
-    dprintf("bitwidth: %d, base: %d, maxx: %d\n", bitwidth, base, maxx);
-    struct Encoder encoder(bitwidth);
-
+    }
+    while(maxx) {
+        bitwidth++;
+        maxx >>= 1;
+    }
+    dprintf("bitwidth: %d\n", bitwidth);
     // actual compression process
+    struct Encoder encoder(bitwidth);
     *res = (U8*)malloc((length * 2) * sizeof(U8));
+    (*res)[encoder.fb++] = bitwidth;
     encoder.origin = *res;
-    int last_correct = 0;
     for(int i = 0; i < length; i++) {
         // dprintf("code %d\n", origin[i]);
         for(int j = bitwidth - 1; j >= 0; j--) {
             encoder.code((origin[i] >> j) & 1);
-        }
-        if(i % 100000 == 0 && i) {
-            dprintf("compress sucessful bit num: %d\n", encoder.correctcnt - last_correct);
-            last_correct = encoder.correctcnt;
         }
     }
     *res_len = encoder.fb + 1;
@@ -286,8 +258,11 @@ void compress(int *origin, size_t length, U8** res, int* res_len) {
 }
 
 void decompress(U8 *origin, size_t length, int* res, int res_len) {
+    bitwidth = origin[0];
     struct Encoder encoder(bitwidth);
+    dprintf("bitwidth: %d\n", bitwidth);
     encoder.origin = origin;
+    encoder.fb = 1;
     for (int i = 0; i < 4; ++i) {
         encoder.x = (encoder.x << 8) + (origin[encoder.fb++] & 255);
     }
@@ -296,17 +271,10 @@ void decompress(U8 *origin, size_t length, int* res, int res_len) {
         for(int j = 0; j < bitwidth; j++) {
             res[i] += res[i] + encoder.decode();
         }
-        res[i] += base;
     }
 }
 
-std::string debugU8ToString(U8 x) {
-    std::string res = "";
-    for(int i = 7; i >= 0; i--) {
-        res += ((x >> i) & 1) + '0';
-    }
-    return res;
-}
+int Mode = 0; // 1 for compress, 0 for decompress
 
 signed main() { 
     int* origin = (int*)malloc(n * sizeof(int));
@@ -317,13 +285,45 @@ signed main() {
         origin[i - 1] = s;
     }
     fclose(in);
-    U8* res;
-    int res_len;
-    compress(origin, n, &res, &res_len);
-    dprintf("original file_size: %d," 
-            "Compressed length: %d,"
-            "total bit num: %lld,"
-            "successful predict bit num: %lld\n", 
-            n * 4, res_len, (U32)n * bitwidth, successcnt);
+    if(Mode) {
+      U8* res;
+      int res_len;
+      time_t start = time(0);
+      compress(origin, n, &res, &res_len);
+      double seconds_since_start = difftime(time(0), start);
+      printf("original file_size: %d," 
+              "Compressed length: %d,"
+              "total bit num: %u,"
+              "successful predict bit num: %u,"
+              "bitwidth: %d,"
+              "time cost: %f\n", 
+              n * 4, res_len, (U32)n * bitwidth, 
+              successcnt, bitwidth,
+              seconds_since_start);
+      FILE* ft = fopen("mae.tmp", "wb");
+      for(int i = 0; i < res_len; i++) {
+          putc(res[i], ft);
+      }
+    } else {
+      FILE* ft = fopen("mae.tmp", "rb");
+      U8* res;
+      int res_len;
+      fseek(ft, 0, SEEK_END);
+      res_len = ftell(ft);
+      fseek(ft, 0, SEEK_SET);
+      res = (U8*)malloc(res_len * sizeof(U8));
+      for(int i = 0; i < res_len; i++) {
+          res[i] = getc(ft);
+      }
+      int* origin1 = (int*)malloc(n * sizeof(int));
+      decompress(res, res_len, origin1, n);
+      for(int i = 0; i < n; i++) {
+            // printf("origin[%d]: %d, origin1[%d]: %d\n", i, origin[i], i, origin1[i]);
+            if(origin[i] != origin1[i]) {
+                printf("origin[%d]: %d, origin1[%d]: %d\n", i, origin[i], i, origin1[i]);
+                break;
+            }
+      }
+    }
     return 0;
 }
